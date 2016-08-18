d5e4e63e0acd413e191b50ea9477db44aa518a27
fantengkong <1228318390@qq.com>
2016-08-18 14:45:21 +0800

    k-2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
 require('./libs/spa.min.js');
 require('./views/index.js');
 require('./views/search.js');
+require('./views/k-register.js');
---
+++ src/scripts/views/k-register.js
@@ -0,0 +1,5 @@
+var indexRegister = require('../tpls/k-register.string');
+
+SPA.defineView('k-register',{
+	html:indexRegister
+});
